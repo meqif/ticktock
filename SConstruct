5665495ea38e645e7da54c0b685b120b1d5a3b9b
Ricardo Martins <ricardo@scarybox.net>
2008-07-03 17:08:51 +0100

    Working time daemon "ticktock".

---
+++ SConstruct
@@ -0,0 +1,11 @@
+"""
+Set up environment.
+"""
+env = Environment(CFLAGS='-Os -Wall')
+
+"""
+Compile the program.
+"""
+env.Program('ticktock.c')
+
+# vim: filetype=python
---
+++ ticktock.c
@@ -0,0 +1,83 @@
+/* See LICENSE file for copyright and license details. */
+/*
+ * ticktock -- a small, secure and light time daemon
+ * Uses the Time protocol (port 37), instead of NTP
+ *
+ * TODO:
+ *   Add DNS resolution
+ *   Add loop for checking and setting the clock
+ *   Read the server from argv
+ *   Getopt
+ */
+
+/* Socket stuff */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define NTP_SERVER "158.152.1.76"
+#define PORT 37
+#define BASE_1970 2208988800UL
+#define SECOND 1000000
+
+int ticktock(const char *hostname, time_t *time_new) {
+    int sockfd, nbytes;
+    unsigned char buf[4];
+    struct sockaddr_in dest_addr;
+
+    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    dest_addr.sin_addr.s_addr = inet_addr(NTP_SERVER);
+    memset(dest_addr.sin_zero, '\0', sizeof dest_addr.sin_zero);
+
+    if (connect(sockfd, (struct sockaddr *)&dest_addr, sizeof dest_addr) == -1) {
+        perror("connect");
+        exit(1);
+    }
+
+    nbytes = recv(sockfd, buf, sizeof buf, 0);
+    buf[nbytes] = 0;
+    *time_new = (time_t)(ntohl(*(uint32_t *)buf) - BASE_1970);
+    printf("Received: %ld\n", *time_new);
+
+    close(sockfd);
+    return 0;
+}
+
+int main() {
+    struct timeval tv;
+    time_t time_new;
+
+    if (gettimeofday(&tv, NULL) == -1)
+        printf("Could not get system time.: %s\n", strerror(errno));
+
+    ticktock(NTP_SERVER, &time_new);
+
+    printf("The clock differs by %ld seconds.\n", tv.tv_sec - time_new);
+
+    tv.tv_sec  = time_new;
+    tv.tv_usec = 0;
+
+    if (settimeofday(&tv, NULL) == -1)
+        printf("Could not set system time: %s\n", strerror(errno));
+
+    //usleep(60 * SECOND);
+
+    return 0;
+}
+
+// vim: et ts=4 sw=4 sts=4
