9ed60c047dc52d479d7436b3b676717416877c23
Ricardo Martins <ricardo@scarybox.net>
2008-07-04 15:08:13 +0100

    Added comments. Exit with error code if time can't be set.

--- ticktock.c
+++ ticktock.c
@@ -30,27 +30,33 @@ int ticktock(const char *hostname, time_t *time_new) {
     unsigned char buf[4];
     struct sockaddr_in dest_addr;
 
+    /* Create socket */
     sockfd = socket(PF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("socket");
         exit(1);
     }
 
+    /* Set socket's properties */
     dest_addr.sin_family = AF_INET;
     dest_addr.sin_port = htons(PORT);
     dest_addr.sin_addr.s_addr = inet_addr(hostname);
     memset(dest_addr.sin_zero, '\0', sizeof dest_addr.sin_zero);
 
+    /* Initiate connection */
     if (connect(sockfd, (struct sockaddr *)&dest_addr, sizeof dest_addr) == -1) {
         perror("connect");
         exit(1);
     }
 
+    /* Get the time */
     nbytes = recv(sockfd, buf, sizeof buf, 0);
     buf[nbytes] = 0;
     *time_new = (time_t)(ntohl(*(uint32_t *)buf) - BASE_1970);
 
+    /* Close socket */
     close(sockfd);
+
     return 0;
 }
 
@@ -67,8 +73,10 @@ int main(int argc, char **argv) {
     tv.tv_sec  = time_new;
     tv.tv_usec = 0;
 
-    if (settimeofday(&tv, NULL) == -1)
+    if (settimeofday(&tv, NULL) == -1) {
         perror("Could not set system time");
+        exit(1);
+    }
 
     return 0;
 }
