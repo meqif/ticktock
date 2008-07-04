19aa27f762682bcd983d874a8eefdbb6b25e2c30
Ricardo Martins <ricardo@scarybox.net>
2008-07-04 22:59:52 +0100

    Reordered alphabetically the imports. Is this a sign of OCD?

--- ticktock.h
+++ ticktock.h
@@ -5,16 +5,16 @@
  * */
 
 /* Network stuff */
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 /* Time setting */
 #include <sys/time.h>
 
 /* System stuff */
-#include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
