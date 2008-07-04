1519ecdb8560ed93fa542ab97047d9ab61694845
Ricardo Martins <ricardo@scarybox.net>
2008-07-04 16:31:39 +0100

    Splint cleaning.

--- ticktock.c
+++ ticktock.c
@@ -13,13 +13,13 @@
 #define SERVICE "time"
 #define BASE_1970 2208988800UL
 
-int ticktock(const char *hostname, time_t *time_new) {
+static int ticktock(const char *hostname, time_t *time_new) {
     int sockfd, nbytes, err;
     unsigned char buf[4];
     struct addrinfo hints, *res;
 
     /* Hints about the type of socket */
-    memset(&hints, '\0', sizeof hints);
+    memset(&hints, 0, sizeof hints);
     hints.ai_family   = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
@@ -27,14 +27,14 @@ int ticktock(const char *hostname, time_t *time_new) {
     err = getaddrinfo(hostname, SERVICE, &hints, &res);
     if (err || res == NULL) {
         printf("Failed to lookup %s\n", hostname);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     /* Create socket */
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sockfd == -1) {
         perror("socket");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     /* Initiate connection */
@@ -42,12 +42,12 @@ int ticktock(const char *hostname, time_t *time_new) {
     if (err == -1) {
         perror("connect");
         freeaddrinfo(res);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     /* Get the time */
-    nbytes = recv(sockfd, buf, sizeof buf, 0);
-    buf[nbytes] = 0;
+    nbytes = (int)recv(sockfd, buf, sizeof buf, 0);
+    buf[nbytes] = '\0';
     *time_new = (time_t)(ntohl(*(uint32_t *)buf) - BASE_1970);
 
     /* Close socket */
@@ -74,7 +74,7 @@ int main(int argc, char **argv) {
 
     if (settimeofday(&tv, NULL) == -1) {
         perror("Could not set system time");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     return 0;
